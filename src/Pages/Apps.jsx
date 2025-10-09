ad0499ca76809e58e2de5a6e4a92468b23210268
Arafat <mdarafat3167@gmail.com>
2025-10-09 18:07:59 +0600

    installation page design

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useApps from '../hooks/useApps';
 import AppCard from '../components/AppCard';
+import { Search } from 'lucide-react';
 
 const Apps = () => {
   const { apps } = useApps();
-  console.log(apps);
+  const [search, setSearch] = useState('');
+
+  const term = search.trim().toLocaleLowerCase();
+
+  const searchApps = term
+    ? apps.filter(app => app.title.toLocaleLowerCase().includes(term))
+    : apps;
 
   return (
     <div className="bg-[#f5f5f5]">
+      <div className="pt-14 pb-8">
+        <h1 className="text-[#001931] text-3xl md:text-4xl text-center font-bold leading-13">
+          Our All Applications
+        </h1>
+        <p className="text-[#627382] text-center px-5 w-full md:w-3xl mx-auto">
+          Explore All Apps on the Market developed by us. We code for Millions
+        </p>
+      </div>
+
+      <div className="container mx-auto px-5 flex justify-between items-center">
+        <p className="text-[#001931] font-semibold">
+          ({searchApps.length}) Apps Found
+        </p>
+        <label className="input">
+          {' '}
+          <Search size={15} color="gray" />
+          <input
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+            type="search"
+            placeholder="Search Products"
+          />
+        </label>
+      </div>
+
       <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5 container mx-auto p-5">
-        {apps.map(app => (
+        {searchApps.map(app => (
           <AppCard key={app.id} app={app}></AppCard>
         ))}
       </div>
--- src/Pages/Installation.jsx
+++ src/Pages/Installation.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Installation = () => {
   return (
-    <div>
-      <h1>Installation Page</h1>
+    <div className="bg-[#f5f5f5]">
+      <div className="pt-14 pb-8">
+        <h1 className="text-[#001931] text-3xl md:text-4xl text-center font-bold leading-13">
+          Your Installed Apps
+        </h1>
+        <p className="text-[#627382] text-center px-5 w-full md:w-3xl mx-auto">
+          Explore All Apps on the Market developed by us. We code for Millions
+        </p>
+      </div>
     </div>
   );
 };
